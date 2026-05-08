11779da73724f6bcf7dedeae0bc54aa07eca5c3e
Cristian Ricaurte <cfricaurte1@gmail.com>
2026-05-08 17:46:58 -0500

    UI: Refactorización Pixel-Perfect de la vista de Login basada en Figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+                roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
             },
         },
     },
